a315280f706a40938b18f085f2d96d4540d49983
unknown <BinaryCompound2@gmail.com>
2023-06-13 14:39:08 +0300

    editing separator, addet animated buttons

--- start.js
+++ start.js
@@ -1,4 +1,13 @@
 'use sctict'
+const toggleElement = () => {
+    const x = document.getElementById("showHide");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+};
+
 
 
 
