22bb703872f678bb741039fd4056585e43ff84a4
unknown <BinaryCompound2@gmail.com>
2023-06-21 19:47:26 +0300

    adding new pages

--- start.js
+++ start.js
@@ -1,18 +1,18 @@
 'use sctict'
-const toggleElement = () => {
+/*const toggleElement = () => {
     const x = document.getElementById("showHide");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
-};
+};*/
 
-function myFunction() {
+function dropDownMenu() {
     const links = document.getElementById("myLinks");
     links.style.display = (links.style.display === "block") ? "none" : "block";
-  }
-  
+}
+
 
 
 
